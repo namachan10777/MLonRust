f4169f1fb180d6650ee094876da5d34f1de40fe4
Nakano Masaki <namachan10777@gmail.com>
2021-01-01 23:01:23 +0900

    lsm_wine_quality

--- examples/lsm_wine_quality.rs
+++ examples/lsm_wine_quality.rs
@@ -22,5 +22,21 @@ fn main() {
             .collect::<Vec<RowVector<f64, Dynamic, _>>>()
             .as_slice(),
     );
-    println!("{:?}", dm.shape());
+    let (nrows, _) = dm.shape();
+
+    let dm_train = dm.index((0..nrows/3*2, ..));
+    let g_train = dm_train.index((.., ..11)).insert_column(0, 1.0);
+    let y_train = dm_train.index((.., 11));
+
+    let dm_test = dm.index((nrows/3*2.., ..));
+    let g_test = dm_test.index((.., ..11)).insert_column(0, 1.0);
+    let y_test = dm_test.index((.., 11));
+
+    let a = (g_train.transpose() * g_train.clone()).try_inverse().unwrap() * g_train.transpose() * y_train;
+
+    let y_estimated = g_test * a;
+    let tss = y_test.lp_norm(2);
+    let rss = (y_test - y_estimated).lp_norm(2);
+    let r2 = 1.0 - rss/tss;
+    println!("{:?}", r2);
 }
